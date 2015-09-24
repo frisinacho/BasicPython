cb6aff115eddc58c18a7dcd433db44782b6dfd67
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-24 17:02:08 +0200

    Getting numbers and making a sum with them

--- Input.py
+++ Input.py
@@ -8,3 +8,7 @@ print "Hello", name
 # Get numbers
 print "Give me a number:"
 num1 = int(raw_input())
+print "Give me another number:"
+num2 = int(raw_input())
+result = num1 + num2
+print "The sum of those number is:", result
