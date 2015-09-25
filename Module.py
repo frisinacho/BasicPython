a96b3d692d7983849fc7c3a1eb0e8e52a9f2c337
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-09-26 00:43:02 +0200

    More stuff with Math

--- Module.py
+++ Module.py
@@ -7,3 +7,12 @@ print math.factorial(4)
 
 # Log
 print math.log1p(2.4)
+
+# More stuff
+print "Give me a number:"
+num = float(raw_input())
+
+factorial = math.factorial(math.floor(num))
+logarithm = math.log1p(num)
+
+print "The log of %f is %f and its factorial is %d" % (num, logarithm, factorial)
