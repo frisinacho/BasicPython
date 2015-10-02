2f0169db88a8ec4bc25d65e0b68cb6191614d199
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-02 16:43:50 +0200

    This program copy itself into 10 directories

--- Worm.py
+++ Worm.py
@@ -6,3 +6,8 @@ import subprocess
 script = argv
 name = str(script[0])
 print name
+
+for i in range(0, 10):
+    directory = 'dir' + str(i)
+    subprocess.call(['mkdir', directory])
+    subprocess.call(['cp', name, directory])
