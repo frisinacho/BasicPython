4ab4ecc2dbf0ca004ab5b09f78b49bba42e9be84
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-02 17:38:22 +0200

    Using CamelCase for class

--- OOP.py
+++ OOP.py
@@ -1,14 +1,14 @@
 __author__ = 'Nacho'
 
 
-class dog:
+class Dog:
     legs = 4
     name = ""
 
     def naming(self, n):
         self.name = n
 
-pet = dog()
+pet = Dog()
 pet.naming("Lola")
 
 print "I've a dog, it's name is %s and have %s legs" % (pet.name, pet.legs)
