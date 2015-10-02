b5d4f25318083d41454440742563bb4744386e0d
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-10-02 17:37:02 +0200

    Creating functions inside class

--- OOP.py
+++ OOP.py
@@ -3,7 +3,12 @@ __author__ = 'Nacho'
 
 class dog:
     legs = 4
-    name = "Lola"
+    name = ""
+
+    def naming(self, n):
+        self.name = n
 
 pet = dog()
+pet.naming("Lola")
+
 print "I've a dog, it's name is %s and have %s legs" % (pet.name, pet.legs)
